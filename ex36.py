62536ad6a4e54992a466a515caa9e27cfebddaf7
Door <dominick.donahue@gmail.com>
2017-04-30 21:06:54 -0500

    Update ex36.py
    added the first intro to the game. Changed the method by which i picked a winner to advance to the next question.

--- ex36.py
+++ ex36.py
@@ -6,22 +6,32 @@
 
 from sys import exit
 
-
-
 # part where you check input against string length:
 # we will either send the player to the intro on the second puzzle,
 # or send them to the intro of the first puzzle
+
+def firstIntro():
+    print """
+    bro you're in the BIG GAME
+    the BIG MAN and you are doing, that ONE-ON-ONE"
+    right now .... you HAVV the rock \n
+    """
+    firstCheck()
+
 def firstCheck():
     print "YOU NEED TO WORFMK YOUR .... FEET"
     choice = raw_input("8:y ")
-    if choice.length == 32:
+    if (len(choice) % 7 == 2) and len(choice) > 3:
         print "Hecp. Your SWEET FOOTWORK is that's got you to"
         print "DUNKEN,, DISTANCE"
-        score += 413
-        secondIntro()
+        # score += 413
+        # secondIntro()
+        exit(0)
     else:
         print "you need to see the BIG MAN'S stop your steps"
         print "aaa hellla he's too nimble for you"
         print "So godam . . . slick"
         firstIntro()
 # end
+
+firstIntro()
